d34bbbcabede206e1971d0ee90a323611f7bdcf2
shubhams <shub6221@gmail.com>
2024-04-05 17:54:05 +0530

    added basics - 25% completion

---
+++ src/htmlnode.py
@@ -0,0 +1,11 @@
+class HTMLNode:
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError()
+
+    
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from textnode import TextNode
+
+node = TextNode("This is a text node", "BOLD" , "NA")
+print(node)
